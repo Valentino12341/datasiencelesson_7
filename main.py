5fc6f4d29da31b3fef9c931caf3762d2181ea2c5
Valentino12341 <105175770+Valentino12341@users.noreply.github.com>
2026-06-23 16:45:25 +0200

    Improve comments and add data preprocessing steps
    Refactor and enhance comments for clarity. Implement data preprocessing steps including handling missing values and encoding categorical variables.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
  # A getting the data and loading it in to the program 
  # B prepare the datta and look if the isnt data missing and change int in to strings of 1 0 for example 
  # C alalyse the data so that you can define the input and what the output is 
- # E train the moddle use for example 75 % of the data to train the moddle and 25 % to test if it works 
+ # E train the model use for example 75 % of the data to train the moddle and 25 % to test if it works 
  # F select de machine learning algorithm and perform the training of the programm 
  # G look at predicions and compare to what the program did to look at how acurate it is 
 
@@ -27,6 +27,45 @@ print("Target : \n",y)
 
 # data preprocessing
 
+
+
+
 # replacing the missing value in salary and in age with the avrage{mean} of the collum 
 from sklearn.impute import SimpleImputer
+
+imputer = SimpleImputer(missing_values=np.nan,strategy="mean")
+
+
+
+x[:,1:3] = imputer.fit_transform(x[:,1:3])
+print(x)
+
+
+#converting all string in to numbers 
+from sklearn.preprocessing import LabelEncoder
+
+encoder = LabelEncoder()
+y = encoder.fit_transform(y)
+
+print(y)
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+
+ct = ColumnTransformer(transformers=[("encoder",OneHotEncoder(),[0])],remainder="passthrough") ####
+x = pd.DataFrame(ct.fit_transform(x))
+print(x)
+
+
+#SPLITINNG the data set in to training and testing 
+from sklearn.model_selection import train_test_split
+
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size= 0.2,random_state = 1)
+
+print("xtrain :\n",x_train)
+print("xtest :\n",x_test)
+print("ytrain :\n",y_train)
+print("ytest :\n",y_test)
+
+
 
